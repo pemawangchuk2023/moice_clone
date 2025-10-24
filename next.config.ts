c3de57a51053199f1449e3117146847730f42d64
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-10-24 14:47:11 +0600

    first commir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	experimental: {
+		turbopackFileSystemCacheForDev: true,
+	},
 };
 
 export default nextConfig;
